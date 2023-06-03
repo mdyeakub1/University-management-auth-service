bf055618b5013a64abea1f56d0d3dbe73ba3545a
yakub9876 <hmdyakub9876@gmail.com>
2023-06-03 18:25:59 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 3000
 
 app.use(cors())
 
@@ -9,9 +8,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res:Response) => {
   res.send('Hello World!')
 })
 
 export default app
-
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
 import mongoose from 'mongoose'
-import config from './config';
+import config from './config'
 import app from './app'
 
-
 async function boostrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Database is connected successfully!")
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-
-        })
-    } catch (error) {
-        console.log("Failed to connect database", error)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected successfully!')
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Failed to connect database', error)
+  }
 }
 boostrap()
